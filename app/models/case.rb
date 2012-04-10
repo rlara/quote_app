99544a95c13ca2e3513cec040c51269514ace6d9
Roberto Lara <rlara@incaztech.com>
2012-04-10 14:57:02 -0500

    validar la columna tipe

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,7 +1,7 @@
 class Case < ActiveRecord::Base
   validates :name,  :presence => true
   validates :description,  :presence => true, :length => { :maximum => 500 }
-
+  validates :tipe, :presence => true
   
   belongs_to :quote
 end
